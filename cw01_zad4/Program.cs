5f6b9854084ef03aa50cf8d224a0959700f5c645
adamsnode <adamsnode@gmail.com>
2026-04-19 16:36:32 +0200

    dodano zadanie 4 - sprawadzanie pelnoletnosci

---
+++ cw01_zad4/Program.cs
@@ -0,0 +1,13 @@
+﻿int aktualnyRok = DateTime.Now.Year;
+Console.WriteLine("Podaj swój rok urodzenia:");
+int rokUrodzenia = int.Parse(Console.ReadLine());
+
+int wiek = aktualnyRok - rokUrodzenia;
+if (wiek >= 18)
+{
+    Console.WriteLine("Jesteś pełnoletni.");
+}
+else
+{
+    Console.WriteLine("Nie jesteś pełnoletni.");
+}
