57dd6d15f3e987f1a7233b49a0d5ad1d8d91815a
adamsnode <adamsnode@gmail.com>
2026-04-19 15:17:59 +0200

    pierwszy commit

--- cw01_/Program.cs
+++ cw01_/Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj nazwę użytkownika: ");
+       string login = Console.ReadLine();
+
+       if (login.Length >= 5)
+        {
+          Console.WriteLine("Nazwa użytkownika poprawna");
+        } 
+        else
+        {
+          Console.WriteLine("Nazwa użytkownika jest za krótka");  
+        }
+
+
+    }
+
+}
