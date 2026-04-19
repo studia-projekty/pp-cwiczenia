de5f58cfaf2078b5aeb522ec557ac727ffb08f17
adamsnode <adamsnode@gmail.com>
2026-04-19 17:18:03 +0200

    dodano zadanie nr 7 - obecnosc pracownikow

---
+++ cw01_zad7/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+
+int iloscPracownikow = 0;
+
+int numerPracownika = 1;
+
+
+for (int i = 1; i <6; i++)
+{
+    Console.WriteLine("Czy pracownik numer " + numerPracownika + " jest obecny? (tak/nie)");
+    string czyObecny1 = Console.ReadLine();
+
+    if (czyObecny1 == "tak")
+    {
+        iloscPracownikow++;
+        numerPracownika++;
+    }
+    if (czyObecny1 == "nie")
+    {
+        numerPracownika++;
+        Console.WriteLine("Pracownik numer " + numerPracownika + " jest nieobecny.");
+    }
+
+}
+
+Console.WriteLine("Liczba obecnych pracowników: " + iloscPracownikow);
