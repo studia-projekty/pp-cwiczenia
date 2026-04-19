788f73c2745b4c5e5205c928d3f890ca52e29f5b
adamsnode <adamsnode@gmail.com>
2026-04-19 16:56:20 +0200

    dodano zadanie nr 5 - numerowanie paczek

---
+++ cw01_zad5/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+Console.WriteLine("Podaj temperaturę:");
+double temperatura = double.Parse(Console.ReadLine());
+
+if (temperatura < 0)
+{   
+    Console.WriteLine("Na zewnątrz jest mróz.");
+
+}
+if (temperatura == 0)
+{
+    Console.WriteLine("Temperatura wynosi zero stopni.");
+}
+if (temperatura > 0)
+{
+    Console.WriteLine("Na zewnątrz jest powyżej zera.");
+}
---
+++ cw01_zad6/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+
+
+for (int nrPaczki = 1; nrPaczki < 6; nrPaczki++)
+{
+    Console.WriteLine("Przygotowano paczkę numer: " + nrPaczki);
+}
