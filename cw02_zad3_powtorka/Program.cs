e79923ab49f518e5e3e9b8bc2cb0cdcb50e7e0ca
adamsnode <adamsnode@gmail.com>
2026-05-11 09:24:07 +0200

    dodano opcje portfel

--- cw02_zad3_powtorka/Program.cs
+++ cw02_zad3_powtorka/Program.cs
@@ -15,7 +15,7 @@ int ileZaMalo = 0;
 
 int portfel = 0;
 
-while (wybor !=4)
+while (wybor != 4)
 {
     Console.WriteLine("Wybierz opcję:");
     Console.WriteLine("1. Kup produkt");
@@ -26,7 +26,7 @@ while (wybor !=4)
     wybor = int.Parse(Console.ReadLine());
 
     if (wybor == 1)
-    {   
+    {
         bool zaMalo = false;
         bool istnieje = false;
 
@@ -45,9 +45,9 @@ while (wybor !=4)
                 // podaj ilosc
                 Console.WriteLine("Podaj ilość:");
                 int podanaIlosc = int.Parse(Console.ReadLine());
-                
+
                 if (ilosc[i] >= podanaIlosc)
-                {   
+                {
                     // odejmij z ilosci wpisana wartosc
                     ilosc[i] -= podanaIlosc;
                     //dodaj koszt transakcji (cena * ilosc) do portfela
@@ -63,24 +63,24 @@ while (wybor !=4)
                     ileZaMalo = ilosc[i];
                     break;
                 }
-                
+
             }
         }
         //jezeli produkt nie istnieje - wyswietl komunikat / jezeli niewystarczajaca ilosc - wyswietl komunikat
         if (zaMalo == true)
         {
             Console.WriteLine("Brak wystarczającej ilości na magazynie: " + ileZaMalo);
-            
+
         }
         if (istnieje == false)
         {
             Console.WriteLine("Produkt o podanej nazwie nie istnieje");
         }
-        
+
     }
     //dodaj produt
     if (wybor == 2)
-    {   
+    {
         bool istnieje = false;
 
         //podaj nazwe produktu
@@ -88,57 +88,63 @@ while (wybor !=4)
         string podanaNazwa = Console.ReadLine();
 
         for (int i = 0; i < nazwa.Length; i++)
-        {   
+        {
             //jezeli istnieje, to podaj liczbe do dodania
-            if(podanaNazwa == nazwa[i])
+            if (podanaNazwa == nazwa[i])
             {
                 istnieje = true;
-                
+
                 //podaj liczbe do dodania
                 Console.WriteLine("Podaj ilość");
                 int podanaIlosc = int.Parse(Console.ReadLine());
 
                 //dodaj podana liczbe do ilosci produktu na stanie
                 ilosc[i] += podanaIlosc;
-                
-                Console.WriteLine("Dodano. Aktualny stan " + nazwa[i] + " to: " + ilosc[i]); 
-                break;           
+
+                Console.WriteLine("Dodano. Aktualny stan " + nazwa[i] + " to: " + ilosc[i]);
+                break;
             }
         }
 
-            // jezeli nie istnieje to podaj liczbe oraz cene
-            if (istnieje == false)
+        // jezeli nie istnieje to podaj liczbe oraz cene
+        if (istnieje == false)
+        {
+            //podaj liczbe do dodania
+            Console.WriteLine("Podaj ilosć do dodania:");
+            int podanaIlosc = int.Parse(Console.ReadLine());
+
+            //podaj cene do dodania
+            Console.WriteLine("Podaj cenę do dodania:");
+            int podanaCena = int.Parse(Console.ReadLine());
+
+            //petla
+            for (int i = 0; i < nazwa.Length; i++)
             {
-                //podaj liczbe do dodania
-                Console.WriteLine("Podaj ilosć do dodania:");
-                int podanaIlosc = int.Parse(Console.ReadLine());
+                //sprawdz czy jest wolne miejsce w tablicy
+                if (nazwa[i] == null)
+                {
+                    // dodaj nazwe, ilosc i cene nowego produktu do tablic
+                    nazwa[i] = podanaNazwa;
+                    ilosc[i] = podanaIlosc;
+                    cena[i] = podanaCena;
 
-                //podaj cene do dodania
-                Console.WriteLine("Podaj cenę do dodania:");
-                int podanaCena = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Dodano: " + nazwa[i] + " " + ilosc[i] + " " + cena[i]);
+                    break;
 
-                //petla
-                for (int i = 0; i < nazwa.Length; i++)
-                {   
-                    //sprawdz czy jest wolne miejsce w tablicy
-                    if (nazwa[i] == null)
-                    {
-                        // dodaj nazwe, ilosc i cene nowego produktu do tablic
-                        nazwa[i] = podanaNazwa;
-                        ilosc[i] = podanaIlosc;
-                        cena[i] = podanaCena;
-
-                        Console.WriteLine("Dodano: " + nazwa[i] + " " + ilosc[i] + " " + cena[i]);
-                        break;
-
-                    }
                 }
-
-
-                
             }
-         
-        
+
+
+
+        }
+
+
     }
+    if (wybor == 3)
+    {
+        //pokaz portfel
+        Console.WriteLine("Pokaż portfel: " + portfel + "\n");
+    }    
+
     
 }
