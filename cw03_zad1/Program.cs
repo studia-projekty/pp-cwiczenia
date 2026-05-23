5a66aa3f330e6dd172bf3ef0dfbe2a171ab05ba8
adamsnode <adamsnode@gmail.com>
2026-05-24 00:09:59 +0200

    cw03 zad01 gotowe

--- cw03_zad1/Program.cs
+++ cw03_zad1/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.CodeAnalysis;
+
+Console.WriteLine("Podaj liczbę n:");
+int liczba = int.Parse(Console.ReadLine());
+int suma = 0;
+
+for(int i=1; i<liczba; i++)
+{
+    suma += i;
+}
+
+Console.WriteLine("");
+Console.WriteLine("Suma liczb od 1 do n wynosi: " + suma); 
