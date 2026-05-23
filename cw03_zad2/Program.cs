8aa9d577ee98e82500cb56b12382cd0d2d48dfc1
adamsnode <adamsnode@gmail.com>
2026-05-24 01:39:31 +0200

    cw03_zad3 gotowe

---
+++ cw03_zad2/Program.cs
@@ -0,0 +1,10 @@
+﻿
+for (int wiersz = 1; wiersz <= 10; wiersz++)
+{
+    for (int kolumna = 1; kolumna <= 10; kolumna++)
+    {       
+        int wynik = wiersz * kolumna;
+
+        Console.WriteLine($"{wiersz} x {kolumna} = {wynik}");
+    }
+}                                         
---
+++ cw03_zad3/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int liczba = 1;
+bool czyParzysta = true;
+int parzysta = 0;
+int nieparzysta = 0;
+
+Console.WriteLine("Liczby Parzyste: ");
+while (parzysta <= 50)
+{
+    if (parzysta % 2 == 0)
+    {
+        Console.WriteLine(parzysta);
+    }
+    parzysta++;
+
+}
+
+Console.WriteLine("Liczby niearzyste: ");
+while (nieparzysta <= 50)
+{
+    if (nieparzysta % 2 != 0)
+    {
+        Console.WriteLine(nieparzysta);
+    }
+    nieparzysta++;
+
+
+}
+
+
+
+
+
