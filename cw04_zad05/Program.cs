170b6aaf75d59715d42dc4bc77b18cf3f1d1a81d
adamsnode <adamsnode@gmail.com>
2026-06-14 21:18:52 +0200

    .

--- cw04_zad05/Program.cs
+++ cw04_zad05/Program.cs
@@ -1,35 +1,57 @@
-﻿
-
-Dictionary<string, int> magazyn = new Dictionary<string, int>
+﻿Dictionary<string, int> magazyn = new Dictionary<string, int>
 {
     { "nazwa1", 10 }
 };
 
+int wybor = 0;
+
+
 while (wybor != 4)
 {
+    Console.WriteLine("1. Dodaj produkt\n2. Stan magazynu\n3. Usun produkt");
+    Console.Write("Wybór: "); wybor = int.Parse(Console.ReadLine());
+    
+
     if (wybor == 1)
+        {
+            Console.WriteLine("\nPodaj nazwę produktu:");
+            string nazwa = Console.ReadLine();
+
+            Console.WriteLine("Podaj ilość:");
+            int ilosc = int.Parse(Console.ReadLine());
+            magazyn.Add(nazwa, ilosc);
+
+            //jesli istnieje to dodaj ilosc
+            if (magazyn.ContainsKey("nazwa1"))
+            {
+                magazyn["nazwa1"] += 5;
+            Console.WriteLine($"Dodano {nazwa} w ilośći {ilosc} \n");
+            }
+
+        }
+
+        if (wybor == 2)
+        {
+            Console.WriteLine("\nStan Magazynu:");
+
+
+            //stan magazynu
+            foreach (var produkt in magazyn)
+            {
+                Console.WriteLine($"Produkt: {produkt.Key}, Ilość: {produkt.Value}\n");
+            }
+
+        }
+
+        if (wybor == 3)
     {
-        Console.WriteLine("Podaj nazwę produktu:");
         string nazwa = Console.ReadLine();
-        Console.WriteLine("Podaj ilość:");
-        int ilosc = int.Parse(Console.ReadLine());
-        magazyn.Add(nazwa, ilosc);
+        Console.WriteLine("\nUsuń produkt. Wpisz nazwę:");
+        magazyn.Remove(nazwa);
 
-     }
-
-    //jesli istnieje to dodaj ilosc
-    if (magazyn.ContainsKey("nazwa1"))
-    {
-        magazyn["nazwa1"] += 5;
+        Console.WriteLine($"Produkt {nazwa} usunięto ");
+        
 
     }
-
-
-    //stan magazynu
-    foreach (var produkt in magazyn)
-    {
-        Console.WriteLine($"Produkt: {produkt.Key}, Ilość: {produkt.Value}");
-    }
-
-
+    
 }
---
+++ cw04_zad06/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+string zdanie = Console.ReadLine();
+string[] slowa = zdanie.Split(' ');
+
+Dictionary<string, int> slownik = new Dictionary<string, int>();
+
+
+//petla lecaca przez tablice zrobiona z rozbitego zdania
+foreach (string slowo in slowa)
+{
+
+    if (slownik.ContainsKey(slowo))
+    {
+        slownik[slowo]++;
+
+    } else
+    {
+        slownik.Add(slowo, 1);
+    }
+}
+
+//petla lecaca przez slownik
+foreach (var slowo in slownik)
+{
+    Console.WriteLine($"Słowo: {slowo.Key}, Ilość wystąpień: {slowo.Value}");
+}
+
+
+
+
+
+
